4a80382410148dc41af9447a8f407a178fa7f310
UnstoppableBRO <anandprakash.dk@gmail.com>
2019-11-04 23:28:36 +0530

    changes

--- anand/settings.py
+++ anand/settings.py
@@ -127,8 +127,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[
-   os.path.join(BASE_DIR, 'static')
+   os.path.join(BASE_DIR, 'anand/static')
 ]
-STATIC_ROOT=os.path.join(BASE_DIR,'assets')
+
 # Activate Django-Heroku.
+
 django_heroku.settings(locals())
